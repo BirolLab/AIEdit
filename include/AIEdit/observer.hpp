6514b33d4574bebfbee1fedb6e748544b726be39
parham-k <pkazemi3@gmail.com>
2022-07-19 22:30:10 -0700

    Fix hash function roll back

--- include/AIEdit/observer.hpp
+++ include/AIEdit/observer.hpp
@@ -16,7 +16,6 @@ private:
   const unsigned frame_size, window_size;
   Signature signature;
   std::vector<btllib::SeedNtHash*> hash_fns;
-  size_t position;
 
   void update_signature();
 
@@ -30,9 +29,7 @@ public:
     , window_size(window_size)
     , signature(frame_size, filter.get_seeds().size())
   {
-    position = 0;
     for (const auto& seed : filter.get_seeds()) {
-      position = std::max(position, seed.size());
       auto* nth = new btllib::SeedNtHash(
         seq, { seed }, filter.get_hash_num(), seed.size());
       hash_fns.push_back(nth);
@@ -46,7 +43,7 @@ public:
   bool next();
 
   [[nodiscard]] const Signature& get_signature() { return signature; }
-  [[nodiscard]] size_t get_position() const { return position; }
+  [[nodiscard]] size_t get_position() const;
 };
 
 }
--- src/AIEdit/observer.cpp
+++ src/AIEdit/observer.cpp
@@ -4,17 +4,18 @@ bool
 ai_edit::Observer::next()
 {
   while (true) {
-    for (const auto& hash_fn : hash_fns) {
-      if (!hash_fn->roll()) {
+    for (size_t i = 0; i < hash_fns.size(); i++) {
+      if (!hash_fns[i]->roll()) {
         return false;
       }
-      if (!filter.contains(hash_fn->hashes())) {
-        hash_fn->roll_back();
+      if (!filter.contains(hash_fns[i]->hashes())) {
+        for (size_t j = 0; j <= i; j++) {
+          hash_fns[j]->roll_back();
+        }
         update_signature();
         return true;
       }
     }
-    ++position;
   }
 }
 
@@ -38,4 +39,25 @@ ai_edit::Observer::update_signature()
       hash_fns[i]->roll();
     }
   }
+}
+
+size_t
+ai_edit::Observer::get_position() const
+{
+  size_t position = hash_fns[0]->get_pos();
+  for (const auto& hash_fn : hash_fns) {
+    if (hash_fn->get_pos() != position) {
+      const std::string red("\033[0;31m");
+      const std::string reset("\033[0m");
+      std::cout << std::endl;
+      std::cout << red << "[ERROR]" << reset
+                << " Hash functions are out of sync: ";
+      for (const auto& nth : hash_fns) {
+        std::cout << nth->get_pos() << " ";
+      }
+      std::cout << std::endl;
+      exit(1);
+    }
+  }
+  return position;
 }
