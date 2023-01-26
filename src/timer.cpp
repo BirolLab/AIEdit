ad2f5e6626b0ebe685a733889b25760d9f693029
parham-k <pkazemi3@gmail.com>
2023-01-25 20:18:39 -0800

    Make timer show wall clock time

--- src/timer.cpp
+++ src/timer.cpp
@@ -5,19 +5,20 @@
 void
 Timer::start()
 {
-    this->t_start = clock();
+    this->t_start = std::chrono::system_clock::now();
 }
 
 void
 Timer::stop()
 {
-    this->t_end = clock();
+    this->t_end = std::chrono::system_clock::now();
 }
 
 long double
 Timer::elapsed_seconds() const
 {
-    return (long double)(this->t_end - this->t_start) / CLOCKS_PER_SEC;
+    std::chrono::duration<double> elapsed = (t_end - t_start);
+    return elapsed.count();
 }
 
 std::string
