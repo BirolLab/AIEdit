22d3fbabcbd0409ea03846fc1448c34a11dc753a
parham-k <pkazemi3@gmail.com>
2023-05-16 22:51:44 -0700

    Remove redundant build script

--- build.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-cmake_prefix_path=$(python -c "import torch;import os;print(os.path.join(torch.utils.cmake_prefix_path, 'Torch'))")
-
-meson setup build --buildtype=release --cmake-prefix-path $cmake_prefix_path $1
-cd build
-ninja 
