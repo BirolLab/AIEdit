fb1e5b4ae231057248ad61e888570f4706ef5abd
parham-k <pkazemi3@gmail.com>
2023-05-23 23:43:52 -0700

    Fix applying fixes

--- src/polisher.cpp
+++ src/polisher.cpp
@@ -15,6 +15,7 @@ PolishingResults Polisher::polish(SequenceIterator& seq_iter)
     while (err_detector.next()) {
         const auto pattern = pattern_detector.get_pattern(seq_iter);
         const auto edits = err_corrector.get_fixes(seq_iter, pattern);
+        seq_iter.previous();
         apply_edits(seq_iter, edits, results);
         const bool fixed = !edits.empty();
         fixed ? ++results.num_fixed_patterns : ++results.num_ignored_patterns;
