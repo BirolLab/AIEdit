34b26210ad63030f477e8a441d82574ef63f1f7d
parham-k <pkazemi3@gmail.com>
2023-06-20 10:45:31 -0700

    Change default seed patterns

--- models/model_trainer/seed_generation.py
+++ models/model_trainer/seed_generation.py
@@ -1,14 +1,11 @@
 def generate_seeds(kmer_length: int, pattern_length: int) -> list[str]:
     seeds = []
-    for i in range(pattern_length):
-        end = "1" + "0" * (pattern_length - i)
-        gap = "0" * (i + 1 + i % 2)
-        cares = "1" * (kmer_length // 2 - len(end) - len(gap) // 2)
-        seed = end + cares + gap + cares + end[::-1]
-        seed = seed[:len(seed) // 2] + seed[len(seed) // 2 + 1 - kmer_length % 2:]
-        seeds.append(seed)
+    for i in range(1, pattern_length + 1):
+        gap = "0" * i + "1" + "0" * i
+        cares = "1" * ((kmer_length - len(gap)) // 2)
+        seeds.append(cares + gap + cares)
     return seeds
 
 
 if __name__ == "__main__":
-    print(*generate_seeds(71, 10), sep='\n')
+    print(*generate_seeds(63, 15), sep='\n')
