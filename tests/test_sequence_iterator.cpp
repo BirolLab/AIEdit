efbc8ef4450a7c8a1162e20a2f3f7de60f64f0ba
parham-k <pkazemi3@gmail.com>
2023-07-06 12:40:01 -0700

    Add more seq iter tests

--- tests/test_sequence_iterator.cpp
+++ tests/test_sequence_iterator.cpp
@@ -6,19 +6,57 @@
 
 TEST_CASE("substitution", "[sequence_iterator]")
 {
-    const std::string seq1 = "ACTGATCGACTGAGCT";
-    const std::string seq2 = "ACTGACCGACTGAGCT";
+    const std::string ref = "ACTGATCGACTGAGCT";
+    const std::string alt = "ACTGACCGACTGAGCT";
     const unsigned position = 5;
 
-    aiedit::SequenceIterator seq_iter1(seq1, {"11011"}, 3);
-    aiedit::SequenceIterator seq_iter2(seq2, {"11011"}, 3);
-    seq_iter1.next();
-    seq_iter2.next();
-    REQUIRE(seq_iter1.get_position() == position);
-    REQUIRE(seq_iter2.get_position() == position);
-    REQUIRE(seq_iter1.get_current() != seq_iter2.get_current());
+    aiedit::SequenceIterator ref_iter(ref, {"11011"}, 1);
+    aiedit::SequenceIterator alt_iter(alt, {"11011"}, 1);
+    ref_iter.next();
+    alt_iter.next();
+    REQUIRE(ref_iter.get_position() == position);
+    REQUIRE(alt_iter.get_position() == position);
+    REQUIRE(ref_iter.get_current() != alt_iter.get_current());
 
-    seq_iter2.substitute_last(seq1[position]);
-    REQUIRE(seq_iter1.get_hashes(0)[0] == seq_iter2.get_hashes(0)[0]);
-    REQUIRE(seq_iter1.get_current() == seq_iter2.get_current());
+    alt_iter.substitute_last(ref[position]);
+    REQUIRE(ref_iter.get_current() == alt_iter.get_current());
+    REQUIRE(ref_iter.get_hashes(0)[0] == alt_iter.get_hashes(0)[0]);
+}
+
+TEST_CASE("insertion", "[sequence_iterator]")
+{
+    const std::string ref = "ACTGACGTACGTAGCTAC";
+    const std::string alt = "ACTGAGTACGTAGCTAC";
+    const unsigned position = 5;
+
+    aiedit::SequenceIterator ref_iter(ref, {"11011"}, 1);
+    aiedit::SequenceIterator alt_iter(alt, {"11011"}, 1);
+    ref_iter.next();
+    alt_iter.next();
+    REQUIRE(ref_iter.get_position() == position);
+    REQUIRE(alt_iter.get_position() == position);
+    REQUIRE(ref_iter.get_current() != alt_iter.get_current());
+
+    alt_iter.insert_last(ref[position]);
+    REQUIRE(ref_iter.get_current() == alt_iter.get_current());
+    REQUIRE(ref_iter.get_hashes(0)[0] == alt_iter.get_hashes(0)[0]);
+}
+
+TEST_CASE("deletion", "[sequence_iterator]")
+{
+    const std::string ref = "ACTGAGTACGTAGCTAC";
+    const std::string alt = "ACTGACGTACGTAGCTAC";
+    const unsigned position = 5;
+
+    aiedit::SequenceIterator ref_iter(ref, {"11011"}, 1);
+    aiedit::SequenceIterator alt_iter(alt, {"11011"}, 1);
+    ref_iter.next();
+    alt_iter.next();
+    REQUIRE(ref_iter.get_position() == position);
+    REQUIRE(alt_iter.get_position() == position);
+    REQUIRE(ref_iter.get_current() != alt_iter.get_current());
+
+    alt_iter.delete_last();
+    REQUIRE(ref_iter.get_current() == alt_iter.get_current());
+    REQUIRE(ref_iter.get_hashes(0)[0] == alt_iter.get_hashes(0)[0]);
 }
