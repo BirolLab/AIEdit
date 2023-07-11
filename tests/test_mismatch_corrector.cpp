24925f58df9430ccf3023517d5196aed5cc550b6
parham-k <pkazemi3@gmail.com>
2023-07-11 09:50:21 -0700

    Add mismatch corrector tests

---
+++ tests/test_mismatch_corrector.cpp
@@ -0,0 +1,45 @@
+#define CATCH_CONFIG_MAIN
+#include <catch2/catch_test_macros.hpp>
+
+#include <btllib/counting_bloom_filter.hpp>
+#include <nthash/nthash.hpp>
+#include <string>
+#include <vector>
+
+#include "mismatch_corrector.hpp"
+#include "pattern.hpp"
+
+void populate(const std::string& seq,
+              const std::vector<std::string>& seeds,
+              btllib::CountingBloomFilter8& bf)
+{
+    aiedit::SequenceIterator seq_iter(seq, seeds, bf.get_hash_num());
+    while (seq_iter.next()) {
+        for (unsigned i = 0; i < seq_iter.get_num_seeds(); i++) {
+            bf.insert(seq_iter.get_hashes(i));
+        }
+    }
+}
+
+TEST_CASE("single mismatch", "[mismatch_corrector]")
+{
+    const std::string ref = "CATCGCGGCAT";
+    const std::string alt = "CATCGTGGCAT";
+    const unsigned position = 5;
+    const std::vector<std::string> seeds = {"11011"};
+
+    btllib::CountingBloomFilter8 bf(128, 3);
+    populate(ref, seeds, bf);
+    aiedit::MismatchCorrector mc(bf);
+
+    aiedit::Pattern pattern(1);
+    pattern.set(0, aiedit::Edit::Type::MISMATCH);
+    aiedit::SequenceIterator seq_iter(alt, seeds, bf.get_hash_num());
+    seq_iter.next();
+    const auto edits = mc.fix(seq_iter, pattern);
+    REQUIRE(edits.size() == 1);
+    REQUIRE(edits.front().position == position);
+    REQUIRE(edits.front().before == alt[position]);
+    REQUIRE(edits.front().after == ref[position]);
+    REQUIRE(edits.front().type == aiedit::Edit::Type::MISMATCH);
+}
