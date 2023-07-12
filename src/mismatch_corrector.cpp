9f02c427ee0e82364206c1ca795796410e4a997d
parham-k <pkazemi3@gmail.com>
2023-07-11 23:57:10 -0700

    Fix mismatch corrector miss detection

--- src/mismatch_corrector.cpp
+++ src/mismatch_corrector.cpp
@@ -12,7 +12,11 @@ inline unsigned count(SequenceIterator& seq_iter, const btllib::CountingBloomFil
 {
     unsigned sum = 0;
     for (unsigned i = 0; i < seq_iter.get_num_seeds(); i++) {
-        sum += bf.contains(seq_iter.get_hashes(i));
+        const auto c = bf.contains(seq_iter.get_hashes(i));
+        if (c == 0) {
+            return 0;
+        }
+        sum += c;
     }
     return sum;
 }
@@ -20,21 +24,18 @@ inline unsigned count(SequenceIterator& seq_iter, const btllib::CountingBloomFil
 inline bool permute(SequenceIterator& seq_iter, const btllib::CountingBloomFilter8& bf)
 {
     char original = seq_iter.get_current();
-    char fix = 'N';
+    char fix = original;
     unsigned max_count = 0;
     for (const auto c : ALPHABET) {
-        if (c == original) {
-            continue;
-        }
         seq_iter.substitute_last(c);
         const auto count_c = count(seq_iter, bf);
-        if (count_c > 0 && count_c > max_count) {
+        if (count_c > max_count) {
             fix = c;
             max_count = count_c;
         }
     }
     seq_iter.substitute_last(fix);
-    return fix;
+    return max_count > 0;
 }
 
 }  // namespace
@@ -44,7 +45,7 @@ namespace aiedit {
 std::vector<Edit> MismatchCorrector::fix(SequenceIterator& seq_iter, const Pattern& pattern)
 {
     std::vector<Edit> edits;
-    const unsigned num_checks = seq_iter.get_seed_length() + pattern.get_length() - 1;
+    const unsigned num_checks = pattern.get_length();
     for (unsigned i = 0; i < num_checks; i++) {
         if (i < pattern.get_length() && pattern.get(i) == Edit::MISMATCH) {
             const auto before = seq_iter.get_current();
@@ -53,13 +54,12 @@ std::vector<Edit> MismatchCorrector::fix(SequenceIterator& seq_iter, const Patte
             if (fixed) {
                 edits.emplace_back(seq_iter.get_position(), Edit::Type::MISMATCH, before, after);
             } else {
-                seq_iter.skip_kmer();
                 return edits;
             }
         } else if (count(seq_iter, bf) == 0) {
-            seq_iter.skip_kmer();
             return edits;
         }
+        seq_iter.next();
     }
     return edits;
 }
