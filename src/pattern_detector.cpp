109132929280d14dec6aab5161d49d54e49416a7
parham-k <pkazemi3@gmail.com>
2023-08-29 12:33:10 -0700

    Removed indels from pattern detector

--- src/pattern_detector.cpp
+++ src/pattern_detector.cpp
@@ -1,15 +1,10 @@
 #include "pattern_detector.hpp"
 
+#include <bitset>
+
 namespace {
 
 using namespace aiedit;
-
-const Edit::Type EDIT_TYPES[4] = {
-  Edit::Type::NONE,
-  Edit::Type::MISMATCH,
-  Edit::Type::INSERTION,
-  Edit::Type::DELETION,
-};
 
 inline fdeep::tensor get_model_input(SequenceIterator seq_iter,
                                      unsigned pattern_length,
@@ -43,18 +38,65 @@ inline unsigned argmax(const fdeep::tensor& x)
     return idx_max;
 }
 
+inline void fill_first(Pattern& pattern, Edit::Type val, unsigned num)
+{
+    for (unsigned i = 0; i < num; i++) {
+        pattern.set(i, val);
+    }
+}
+
+inline unsigned get_num_insertions(SequenceIterator seq_iter,
+                                   unsigned pattern_length,
+                                   const btllib::CountingBloomFilter8& bf,
+                                   const fdeep::model& model)
+{
+    for (unsigned num_ins = 1; num_ins <= pattern_length; num_ins++) {
+        seq_iter.insert_last('N');
+        const auto signature = get_model_input(seq_iter, pattern_length, bf);
+        const auto model_output = model.predict({signature});
+        const auto argmax_y = argmax(model_output[0]);
+        if (argmax_y > 0) {
+            return num_ins;
+        }
+    }
+    return 0;
+}
+
+inline unsigned get_num_deletions(SequenceIterator seq_iter,
+                                  unsigned pattern_length,
+                                  const btllib::CountingBloomFilter8& bf)
+{
+    seq_iter.next(seq_iter.get_seed_length());
+    for (unsigned num_del = 1; num_del <= pattern_length; num_del++) {
+        bool fixed = true;
+        for (unsigned i = 0; i < seq_iter.get_num_seeds() && fixed; i++) {
+            if (bf.contains(seq_iter.get_hashes(i)) == 0) {
+                fixed = false;
+            }
+        }
+        if (fixed) {
+            return num_del;
+        }
+        seq_iter.next();
+    }
+    return 0;
+}
+
 }  // namespace
 
 namespace aiedit {
 
 Pattern PatternDetector::get_pattern(SequenceIterator& seq_iter)
 {
-    const auto& signature = get_model_input(seq_iter, pattern_length, bf);
-    auto model_output = model.predict({signature});
+    const auto signature = get_model_input(seq_iter, pattern_length, bf);
+    const auto model_output = model.predict({signature});
+    const auto argmax_y = argmax(model_output[0]);
+    const std::string pattern_string = std::bitset<64>(argmax_y).to_string();
     Pattern pattern(pattern_length);
     for (unsigned i = 0; i < pattern.get_length(); i++) {
-        const auto argmax_y = argmax(model_output[i]);
-        pattern.set(i, EDIT_TYPES[argmax_y]);
+        if (pattern_string[pattern_string.size() - i - 1] == '1') {
+            pattern.set(i, Edit::Type::MISMATCH);
+        }
     }
     return pattern;
 }
