b965c4dc2b2623307059dbaecca43e03576e3500
parham-k <pkazemi3@gmail.com>
2023-09-27 20:37:20 -0700

    Fix seq iter tests

--- tests/test_sequence_iterator.cpp
+++ tests/test_sequence_iterator.cpp
@@ -79,6 +79,6 @@ TEST_CASE("deletion", "[sequence_iterator]")
 
     alt_iter.delete_last();
     REQUIRE(ref_iter.get_current() == alt_iter.get_current());
-    REQUIRE(ref_iter.get_position() == alt_iter.get_position());
+    REQUIRE(ref_iter.get_position() == alt_iter.get_position() - 1);
     REQUIRE(ref_iter.get_hashes(0)[0] == alt_iter.get_hashes(0)[0]);
 }
