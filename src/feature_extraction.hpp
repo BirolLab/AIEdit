91582c6081243eb94bb516e815cfa357da60bfab
parham-k <pkazemi3@gmail.com>
2024-12-03 12:22:39 -0800

    Switch to Seq2seq model

--- src/feature_extraction.hpp
+++ src/feature_extraction.hpp
@@ -20,30 +20,30 @@ torch::Tensor get_model_input(const std::string& seq,
 {
     const auto k = seeds[0].size();
     const auto num_hashes = cbf.get_hash_num();
-    auto x = torch::ones({(unsigned)seeds.size() + max_indels * 2 + 1, end - start});
+    auto x = torch::ones({end - start, (unsigned)seeds.size() + max_indels * 2 + 1});
     btllib::SeedNtHash nh(seq, seeds, num_hashes, k, start);
     while (nh.roll() && nh.get_pos() < end) {
         for (unsigned i = 0; i < seeds.size(); i++) {
             const auto hashes = nh.hashes() + i * num_hashes;
-            x[i + 1][nh.get_pos() - start] = error_probs[cbf.contains(hashes)];
+            x[nh.get_pos() - start][i + 1] = error_probs[cbf.contains(hashes)];
         }
     }
     aiedit::DeleteGapHash dh(seq, num_hashes, k, max_indels, start);
     char prev = 0;
-    while (dh.roll() && dh.get_pos() < end - k / 2) {
-        char curr = std::toupper(seq[dh.get_pos() - start + k / 2]);
-        x[0][dh.get_pos() - start + k / 2] = curr == prev ? 0.0 : 1.0;
+    while (dh.roll() && dh.get_pos() < end) {
+        char curr = std::toupper(seq[dh.get_pos() - start]);
+        x[dh.get_pos() - start][0] = curr == prev ? 0.0 : 1.0;
         prev = curr;
         for (unsigned i = 0; i < max_indels; i++) {
             const auto prob = error_probs[cbf.contains(dh.hashes()[i])];
-            x[i + seeds.size() + 1][dh.get_pos() - start + k / 2] = prob;
+            x[dh.get_pos() - start][i + seeds.size() + 1] = prob;
         }
     }
     aiedit::InsertGapHash ih(seq, num_hashes, k, max_indels, start);
-    while (ih.roll() && ih.get_pos() < end - k / 2) {
+    while (ih.roll() && ih.get_pos() < end) {
         for (unsigned i = 0; i < max_indels; i++) {
             const auto prob = error_probs[cbf.contains(ih.hashes()[i])];
-            x[i + seeds.size() + max_indels + 1][ih.get_pos() - start + k / 2] = prob;
+            x[ih.get_pos() - start][i + seeds.size() + max_indels + 1] = prob;
         }
     }
     return x;
--- src/train.py
+++ src/train.py
@@ -1,4 +1,5 @@
 import argparse
+import math
 import os
 import re
 import warnings
@@ -7,8 +8,69 @@ import btllib
 import pandas as pd
 import torch
 import torchinfo
-
 from aiedit_torch_extensions import get_model_input
+
+
+def positional_encoding(max_length: int, dim: int):
+    position = torch.arange(max_length).unsqueeze(1)
+    div_term = torch.arange(0, dim, 2)
+    div_term = torch.exp(div_term * (-math.log(10000.0) / dim))
+    pos_enc = torch.zeros(max_length, dim)
+    pos_enc[:, 0::2] = torch.sin(position * div_term)
+    if dim % 2 != 0:
+        pos_enc[:, 1::2] = torch.cos(position * div_term)[:, 0:-1]
+    else:
+        pos_enc[:, 1::2] = torch.cos(position * div_term)
+    return pos_enc
+
+
+class Model(torch.nn.Module):
+
+    def __init__(
+        self,
+        num_seeds: int,
+        max_indels: int,
+        max_length: int = 1000,
+    ):
+        super(Model, self).__init__()
+        model_dim = num_seeds + 2 * max_indels + 1
+        self.__probs_enc = positional_encoding(max_length, model_dim)
+        self.__edits_enc = positional_encoding(max_length, 5)
+        self.register_buffer("probs_enc", self.__probs_enc)
+        self.register_buffer("edits_enc", self.__edits_enc)
+        self.__probs_attn = torch.nn.MultiheadAttention(model_dim, 1)
+        self.__seeds_attn = torch.nn.MultiheadAttention(
+            embed_dim=model_dim,
+            num_heads=1,
+            kdim=num_seeds,
+            vdim=num_seeds,
+        )
+        self.__edits_attn = torch.nn.MultiheadAttention(
+            embed_dim=5,
+            num_heads=1,
+            kdim=model_dim,
+            vdim=model_dim,
+        )
+        self.__masked_attn = torch.nn.MultiheadAttention(5, 1)
+        self.__probs_norm = torch.nn.LayerNorm(model_dim)
+        self.__seeds_norm = torch.nn.LayerNorm(model_dim)
+        self.__edits_norm = torch.nn.LayerNorm(5)
+        self.__out = torch.nn.Linear(5, 5)
+        self.__input_sizes = [(100, model_dim), (25, num_seeds), (max_indels, 5)]
+
+    def summary(self):
+        torchinfo.summary(self, input_size=self.__input_sizes)
+
+    def forward(self, x_probs, x_seeds, x_edits):
+        x_s = x_probs + self.__probs_enc[: x_probs.size(0), :]
+        x_s = self.__probs_norm(x_s + self.__probs_attn(x_s, x_s, x_s)[0])
+        x_s = self.__seeds_norm(x_s + self.__seeds_attn(x_s, x_seeds, x_seeds)[0])
+        mask = torch.ones(x_edits.size(0), x_edits.size(0))
+        mask = torch.triu(mask * float("-inf"), diagonal=1)
+        x_t = x_edits + self.__edits_enc[: x_edits.size(0), :]
+        x_t, _ = self.__masked_attn(x_t, x_t, x_t, attn_mask=mask, is_causal=True)
+        y = self.__edits_norm(x_t + self.__edits_attn(x_t, x_s, x_s)[0])
+        return self.__out(y)
 
 
 def parse_args():
@@ -28,7 +90,11 @@ def read_seeds(path: str) -> list[str]:
     with open(path) as file:
         seeds = [line.strip() for line in file]
     assert len(set(map(len, seeds))) == 1, "seeds should be the same length"
-    return seeds
+    x_seeds = torch.empty(len(seeds[0]), len(seeds))
+    for i in range(x_seeds.size(0)):
+        for j in range(x_seeds.size(1)):
+            x_seeds[i, j] = int(seeds[j][i])
+    return seeds, x_seeds
 
 
 def read_vars(path: str) -> tuple[int, dict[str, pd.DataFrame]]:
@@ -47,64 +113,39 @@ def read_histogram(path: str) -> pd.DataFrame:
     return hist
 
 
-def load_model(
-    num_seeds: int,
-    max_ind: int,
-    path: str,
-) -> tuple[torch.nn.Module, torch.optim.AdamW]:
-    model = torch.nn.Sequential(
-        torch.nn.Conv1d(num_seeds + 2 * max_ind + 1, 64, 7, padding="same"),
-        torch.nn.ReLU(),
-        torch.nn.Conv1d(64, 128, 7, padding="same"),
-        torch.nn.ReLU(),
-        torch.nn.Conv1d(128, 64, 7, padding="same"),
-        torch.nn.ReLU(),
-        torch.nn.Conv1d(64, max_ind + 3, 7, padding="same"),
-    )
-    optimizer = torch.optim.AdamW(model.parameters())
-    if os.path.isfile(path):
-        checkpoint = torch.load(path, weights_only=True)
-        model.load_state_dict(checkpoint["model_state_dict"])
-        optimizer.load_state_dict(checkpoint["optimizer_state_dict"])
-    return model, optimizer
-
-
-def print_model_summary(model):
-    num_channels = next(model.parameters()).size(1)
-    torchinfo.summary(model, input_size=(1, num_channels, 100))
-
-
-def get_targets(vars: pd.DataFrame, seq_len: int, k: int, max_indels):
-    y = torch.zeros(max_indels + 3, seq_len)
-    y[0, :] = 1.0
-    pos_diff = 0
+def get_targets(vars: pd.DataFrame, pos_diff: int):
+    end_pos = vars.iloc[-1]["Seq_pos"] + vars.iloc[-1]["error_length"]
+    num_edits = end_pos - vars.iloc[0]["Seq_pos"]
+    targets = torch.zeros(num_edits + 2, 5)
+    targets[-1, 4] = 1.0
     for _, row in vars.iterrows():
-        seq_pos = row["Seq_pos"] - vars.iloc[0]["Seq_pos"] + pos_diff + 1
+        seq_pos = row["Seq_pos"] - vars.iloc[0]["Seq_pos"]
         if row["error_type"] == "mis":
-            y[0, seq_pos : seq_pos + row["error_length"]] = 0.0
-            y[1, seq_pos : seq_pos + row["error_length"]] = 1.0
+            targets[seq_pos : seq_pos + row["error_length"], 0] = 0.0
+            targets[seq_pos : seq_pos + row["error_length"], 1] = 1.0
         elif row["error_type"] == "ins":
-            y[0, seq_pos : seq_pos + row["error_length"]] = 0.0
-            y[2, seq_pos : seq_pos + row["error_length"]] = 1.0
+            targets[seq_pos : seq_pos + row["error_length"], 0] = 0.0
+            targets[seq_pos : seq_pos + row["error_length"], 2] = 1.0
             pos_diff += row["error_length"]
         elif row["error_type"] == "del":
-            num_ins = min(row["error_length"], max_indels)
-            y[0, seq_pos - 1] = 0.0
-            y[num_ins + 2, seq_pos - 1] = 1.0
+            targets[seq_pos : seq_pos + row["error_length"], 0] = 0.0
+            targets[seq_pos : seq_pos + row["error_length"], 3] = 1.0
             pos_diff -= row["error_length"]
-    return y
+    return targets, pos_diff
 
 
-def filter_samples(x, y, k):
-    mask = torch.zeros(x.size(1)).bool()
-    num_clean = torch.nn.functional.conv1d(
-        y[0, :].unsqueeze(0).unsqueeze(0),
-        torch.ones(1, 1, k),
-    ).squeeze()
-    for start in torch.where(num_clean < k)[0]:
-        start = start.item()
-        mask[max(0, start - k) : start + k] = True
-    return x[:, mask], y[:, mask]
+def validiate_sample(x, y, num_seeds: int) -> bool:
+    max_indels = x.size(1) - 1 - num_seeds
+    x_seeds = x[:, 1 : num_seeds + 1]
+    x_ins = x[:, -2 * max_indels : -max_indels]
+    x_del = x[:, -max_indels:]
+    if y[:, 1].any() and not (x_seeds < 0.5).any():
+        return False
+    if y[:, 2].any() and not (x_ins < 0.5).any():
+        return False
+    if y[:, 3].any() and not (x_del < 0.5).any():
+        return False
+    return True
 
 
 def generate_data(
@@ -131,14 +172,15 @@ def generate_data(
         pos_ends = (read_vars["Seq_pos"] + read_vars["error_length"]).shift().fillna(0)
         read_vars["group"] = (read_vars["Seq_pos"] - pos_ends > 2 * k).cumsum()
         x_read, y_read = [], []
+        pos_diff = 0
         for _, group in read_vars.groupby("group"):
-            start = max(group.iloc[0]["Seq_pos"] - k + 1, 0)
-            end = group.iloc[-1]["Seq_pos"] + group.iloc[-1]["error_length"]
-            start, end = int(start), int(end)
+            start = max(group.iloc[0]["Seq_pos"] - k + 1, 0) + pos_diff
+            end = group.iloc[-1]["Seq_pos"] + group.iloc[-1]["error_length"] + pos_diff
             x = get_model_input(record.seq, start, end, seeds, max_ind, int(cbf), probs)
-            y = get_targets(group, end - start, k, max_ind)
-            x_read.append(x)
-            y_read.append(y)
+            y, pos_diff = get_targets(group, pos_diff)
+            if validiate_sample(x, y, len(seeds)):
+                x_read.append(x)
+                y_read.append(y)
         yield record.id, x_read, y_read
 
 
@@ -148,15 +190,14 @@ def weighted_ce_loss(logits, targets, reduction_factor=0.01):
     return ce_loss.mean()
 
 
-def train(model, optimizer, read_id, x, y, num_epochs):
+def train(model, optimizer, read_id, x, x_seeds, y, num_epochs):
     loss_history = []
     for i in range(num_epochs):
         epoch_loss = 0
-        for x_batch, y_batch in zip(x, y):
+        for x_batch, y_true in zip(x, y):
             optimizer.zero_grad()
-            y_true = y_batch.permute(1, 0).argmax(dim=1)
-            y_pred = model(x_batch).permute(1, 0)
-            loss = weighted_ce_loss(y_pred, y_true)
+            y_pred = model(x_batch, x_seeds, y_true)[:-1, :]
+            loss = weighted_ce_loss(y_pred, y_true[1:, :].argmax(dim=1))
             loss.backward()
             optimizer.step()
             epoch_loss += loss.item()
@@ -168,10 +209,14 @@ def train(model, optimizer, read_id, x, y, num_epochs):
 
 def main():
     args = parse_args()
-    seeds = read_seeds(args.s)
+    seeds, x_seeds = read_seeds(args.s)
     hist = read_histogram(args.m)
-    model, optimizer = load_model(len(seeds), args.i, args.o)
-    print_model_summary(model)
+    model = Model(len(seeds), args.i)
+    optimizer = torch.optim.AdamW(model.parameters())
+    if os.path.isfile(args.o):
+        checkpoint = torch.load(args.o, weights_only=True)
+        model.load_state_dict(checkpoint["model_state_dict"])
+        optimizer.load_state_dict(checkpoint["optimizer_state_dict"])
     print("Reading variants file...")
     num_vars, vars = read_vars(args.e)
     print(f"Number of variants = {num_vars}")
@@ -182,7 +227,8 @@ def main():
     print("Seeds:", *seeds, sep=os.linesep)
     data_generator = generate_data(args.r, vars, cbf, hist, seeds, args.i)
     for read_id, x, y in data_generator:
-        train(model, optimizer, read_id, x, y, args.n)
+        continue
+        train(model, optimizer, read_id, x, x_seeds, y, args.n)
         state_dict = {
             "model_state_dict": model.state_dict(),
             "optimizer_state_dict": optimizer.state_dict(),
